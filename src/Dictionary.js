1618ddc8f57a1f5cc532341766a05fc79034a7df
dijneal <133080518+dijneal@users.noreply.github.com>
2023-09-18 21:15:15 -0700

    created a State to display definition 'meanings'

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [input, setInput] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -26,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={inputResponse} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
